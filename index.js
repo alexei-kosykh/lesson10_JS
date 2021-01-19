874889b6f80e59ba42c853eb49586ce22475a56e
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-01-19 14:45:53 +0300

    some problems with Draw and Delete

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
-// Пустой массив. При нажатии  кнопки push title и description. С помощью этого отрисовываем доску
-// чтобы удалить узнать какое значение у title и description
-// Всегда используется функция перерисовки
-// У карточек один класс
-// забрать title из карточки textContent забирает
-
-// arr.push (title,descr) - здесь все карточки
 // массив перебираем foreach(item) => desk.innerHTML += div class = card и находим
 // closest (id card)
 
 // чтобы удалить нужно найти карточку и затем отрисовать доску заново
 
 const addCard = document.querySelector("#addCard");
+const desk = document.querySelector("#desk");
 const form = document.querySelector("#form");
 
 let userData = [];
 
 const pushData = (title, desc) => {
-  userData.push({ title: title.value, description: desc.value });
+  userData.push("Title: " + title.value);
+  userData.push("Description: " + desc.value);
+  console.log(userData);
 };
 //Создаём функционал для вывода информации из массива на доску. Каждый элемент массива должен добавляться на доску в виде блока с заголовком и описанием.  array.forEach((arr) => {});
 
 const drawCard = () => {
-  const desk = document.querySelector("#desk");
-  let card = document.createElement("div");
-  let cardInfo = document.createElement("div");
-  let cardButton = document.createElement("div");
-  let btnDelete = document.createElement("button");
-  let btnEdit = document.createElement("button");
-  let pTitle = document.createElement("p");
-  let pDesc = document.createElement("p");
-  let pDate = document.createElement("p");
-  const choice = userData[userData.length - 1];
   let date = new Date();
+  let drawCard = {
+    card: document.createElement("div"),
+    cardInfo: document.createElement("div"),
+    cardButton: document.createElement("div"),
+    btnDelete: document.createElement("button"),
+    btnEdit: document.createElement("button"),
+    pTitle: document.createElement("p"),
+    pDesc: document.createElement("p"),
+    pDate: document.createElement("p"),
+  };
 
-  card.className = "desk__card";
-  cardInfo.className = "desk__info";
+  drawCard.card.className = "desk__card";
+  drawCard.cardInfo.className = "desk__info";
 
-  cardButton.className = "buttons__card";
-  btnDelete.className = "button button__card";
-  btnDelete.id = "btnDelete";
-  btnDelete.innerText = "Delete";
+  drawCard.cardButton.className = "buttons__card";
+  drawCard.btnDelete.className = "button button__card";
+  drawCard.btnDelete.id = "btnDelete";
+  drawCard.btnDelete.innerText = "Delete";
 
-  btnEdit.className = "button button__card";
-  btnEdit.id = "btnEdit";
-  btnEdit.innerText = "Edit";
+  drawCard.btnEdit.className = "button button__card";
+  drawCard.btnEdit.id = "btnEdit";
+  drawCard.btnEdit.innerText = "Edit";
 
-  desk.append(card);
-  card.append(cardInfo);
-  pTitle.append("Title: ", choice.title);
-  pDesc.append("Description: ", choice.description);
-  pDate.append(
-    "Data: ",
-    date.getDate(),
-    "-",
-    date.getMonth() + 1,
-    "-",
-    date.getFullYear(),
-    ", ",
-    date.getHours(),
-    ":",
-    date.getMinutes(),
-    ":",
-    date.getSeconds()
-  );
-  cardInfo.append(pTitle, pDesc, pDate);
-  cardInfo.append(cardButton);
-  cardButton.append(btnDelete, btnEdit);
+  userData.forEach((elem, index) => {
+    desk.append(drawCard.card);
+    drawCard.card.append(drawCard.cardInfo);
+    if (index % 2) {
+      drawCard.pDesc.innerHTML = elem;
+      drawCard.pDate.append(
+        "Data: " +
+          date.getDate() +
+          "-" +
+          date.getMonth() +
+          1 +
+          "-" +
+          date.getFullYear() +
+          ", " +
+          date.getHours() +
+          ":" +
+          date.getMinutes() +
+          ":" +
+          date.getSeconds()
+      );
+    } else {
+      drawCard.pTitle.innerHTML = elem;
+    }
+    console.log(userData);
+    drawCard.cardInfo.append(drawCard.pTitle, drawCard.pDesc, drawCard.pDate);
+    drawCard.cardInfo.append(drawCard.cardButton);
+    drawCard.cardButton.append(drawCard.btnDelete, drawCard.btnEdit);
+  });
 
   form.reset();
 };
@@ -77,7 +80,37 @@ addCard.addEventListener("click", (event) => {
   const description = document.querySelector("#description");
 
   pushData(title, description);
-  console.log(userData);
 
-  drawCard(userData);
+  drawCard();
+});
+
+desk.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log(event.target);
+  if (event.target.closest("#btnDelete")) {
+    const buttonsCard = event.target.parentElement;
+    const cardInfo = buttonsCard.parentElement;
+    const card = cardInfo.parentElement;
+
+    const pTitle = cardInfo.children[0].innerText;
+    const pDesc = cardInfo.children[1].innerText;
+    let count = 0;
+
+    userData.forEach((elem, index) => {
+      if (pTitle === elem && pDesc === userData[index + 1]) {
+        userData.splice(count, 2);
+
+        return;
+      }
+      count += 1;
+    });
+
+    desk.innerHTML = "";
+    desk.appendChild;
+
+    drawCard();
+  } else {
+    console.log("Edit btn");
+  }
 });

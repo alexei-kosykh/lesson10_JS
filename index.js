81fcfaf5218c60ee0d9a4c20ee6160ef56eb6763
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-01-20 14:15:41 +0300

    +EditBtn and modal; update Delete

--- index.js
+++ index.js
@@ -1,116 +1,133 @@
-// массив перебираем foreach(item) => desk.innerHTML += div class = card и находим
-// closest (id card)
-
-// чтобы удалить нужно найти карточку и затем отрисовать доску заново
-
 const addCard = document.querySelector("#addCard");
 const desk = document.querySelector("#desk");
 const form = document.querySelector("#form");
 
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
+const btnModalClose = document.querySelector("#btnModalClose");
+const btnModalApply = document.querySelector("#btnModalApply");
+const inputTitle = document.querySelector("#modal-title");
+const inputDescription = document.querySelector("#modal-description");
+
+const date = new Date();
+
+let storageIndex = 0;
 let userData = [];
+// Меньше глобальных переменных!
 
-const pushData = (title, desc) => {
-  userData.push("Title: " + title.value);
-  userData.push("Description: " + desc.value);
-  console.log(userData);
-};
-//Создаём функционал для вывода информации из массива на доску. Каждый элемент массива должен добавляться на доску в виде блока с заголовком и описанием.  array.forEach((arr) => {});
-
-const drawCard = () => {
-  let date = new Date();
-  let drawCard = {
-    card: document.createElement("div"),
-    cardInfo: document.createElement("div"),
-    cardButton: document.createElement("div"),
-    btnDelete: document.createElement("button"),
-    btnEdit: document.createElement("button"),
-    pTitle: document.createElement("p"),
-    pDesc: document.createElement("p"),
-    pDate: document.createElement("p"),
-  };
-
-  drawCard.card.className = "desk__card";
-  drawCard.cardInfo.className = "desk__info";
-
-  drawCard.cardButton.className = "buttons__card";
-  drawCard.btnDelete.className = "button button__card";
-  drawCard.btnDelete.id = "btnDelete";
-  drawCard.btnDelete.innerText = "Delete";
-
-  drawCard.btnEdit.className = "button button__card";
-  drawCard.btnEdit.id = "btnEdit";
-  drawCard.btnEdit.innerText = "Edit";
-
-  userData.forEach((elem, index) => {
-    desk.append(drawCard.card);
-    drawCard.card.append(drawCard.cardInfo);
-    if (index % 2) {
-      drawCard.pDesc.innerHTML = elem;
-      drawCard.pDate.append(
-        "Data: " +
-          date.getDate() +
-          "-" +
-          date.getMonth() +
-          1 +
-          "-" +
-          date.getFullYear() +
-          ", " +
-          date.getHours() +
-          ":" +
-          date.getMinutes() +
-          ":" +
-          date.getSeconds()
-      );
-    } else {
-      drawCard.pTitle.innerHTML = elem;
-    }
-    console.log(userData);
-    drawCard.cardInfo.append(drawCard.pTitle, drawCard.pDesc, drawCard.pDate);
-    drawCard.cardInfo.append(drawCard.cardButton);
-    drawCard.cardButton.append(drawCard.btnDelete, drawCard.btnEdit);
+// Функция отрисовки карточки
+const drawCards = () => {
+  desk.innerHTML = ""; // Каждый раз пишем заново, исходя из массива
+  userData.forEach((item) => {
+    desk.innerHTML += `<div class="desk__card">
+    <div class="desk__info"> 
+    <p>Title: <span class="card__title">${item.title}</span></p>
+    <p>Description: <span class="card__description">${
+      item.description
+    }</span></p>
+    <p>Date: <span class="card__date">${date.getDate()}${
+      date.getMonth() - 1
+    }.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}</span></p>
+    <div class="buttons__card">
+    <button id="btnDelete" class="button button__card">Delete</button>
+    <button id="btnEdit" class="button button__card">Edit</button>
+    </div>
+    </div></div>`;
   });
-
-  form.reset();
 };
 
+// Функция добавления карточки
 addCard.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const title = document.querySelector("#title");
-  const description = document.querySelector("#description");
+  const title = document.querySelector("#title").value;
+  const description = document.querySelector("#description").value;
 
-  pushData(title, description);
+  userData.push({ title: title, description: description });
 
-  drawCard();
+  console.log(userData);
+
+  drawCards();
+
+  form.reset();
 });
 
-desk.addEventListener("click", (event) => {
+// Функция удаления
+const deleteCard = (event) => {
+  // Нужно найти заголовок и описание
+  const card = event.target.closest(".desk__card");
+  // найти внутри другого элемента
+  const title = card.querySelector(".card__title").textContent;
+  const description = card.querySelector(".card__description").textContent;
+
+  // Далее: forEach либо findIndex (2 способа)
+  // Способ findIndex
+  // Находим индекс чтобы использовать splice
+  const index = userData.findIndex(
+    // findIndex - принимает calback первый элемент массива и дальше сравниваем
+    (elem) => elem.title === title && elem.description === description
+  );
+  userData.splice(index, 1); // удаляем 1 элемент
+
+  drawCards(); // отрисовываем
+};
+
+// =======================
+// Все по модалке
+const openModal = () => (modal.style.display = "block");
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+// ========================
+
+// Функция редактирования
+const editCard = (event) => {
+  const card = event.target.closest(".desk__card");
+  const title = card.querySelector(".card__title").textContent;
+  const description = card.querySelector(".card__description").textContent;
+
+  inputTitle.value = title;
+  inputDescription.value = description;
+
+  storageIndex = userData.findIndex(
+    (elem) => elem.title === title && elem.description === description
+  );
+
+  openModal();
+};
+
+const editApply = (event) => {
   event.preventDefault();
 
-  console.log(event.target);
+  userData.splice(storageIndex, 1, {
+    title: inputTitle.value,
+    description: inputDescription.value,
+  });
+
+  closeModal();
+
+  drawCards();
+};
+
+modal.addEventListener("click", (event) => {
+  if (event.target.closest("#btnModalApply")) {
+    editApply(event);
+  } else if (
+    !event.target.closest(".modal__wrapper") ||
+    event.target.closest("#btnModalClose")
+  ) {
+    event.preventDefault();
+    closeModal();
+  }
+});
+
+// Делегирование событий
+desk.addEventListener("click", (event) => {
   if (event.target.closest("#btnDelete")) {
-    const buttonsCard = event.target.parentElement;
-    const cardInfo = buttonsCard.parentElement;
-    const card = cardInfo.parentElement;
-
-    const pTitle = cardInfo.children[0].innerText;
-    const pDesc = cardInfo.children[1].innerText;
-    let count = 0;
-
-    userData.forEach((elem, index) => {
-      if (pTitle === elem && pDesc === userData[index + 1]) {
-        userData.splice(count, 2);
-
-        return;
-      }
-      count += 1;
-    });
-
-    desk.innerHTML = "";
-    desk.appendChild;
-
-    drawCard();
+    // delete func
+    deleteCard(event);
   } else {
-    console.log("Edit btn");
+    editCard(event);
   }
 });
